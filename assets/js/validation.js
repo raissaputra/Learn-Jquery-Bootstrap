d3f03e5bc9e42e5bd99b0f00deeb81e382bb4813
raissaputra <raissaputra02@gmail.com>
2021-09-05 07:15:33 +0700

    validasi pada first name

---
+++ assets/js/validation.js
@@ -0,0 +1,23 @@
+$(ducument).ready(function () {
+  $("#pendaftaran").submit(function (event) {
+    var fname = $("#firstname").val();
+    var lname = $("lastname").val();
+
+    // panggil fungsi
+    val_name(fname, event, "#firstname-error");
+  });
+
+  function val_name(name, event, error) {
+    if (!cekAngka(name)) {
+      $(error).text("Nama tidak boleh kosong atau angka!");
+      event.preventDefault();
+    } else {
+      $(error).text("");
+    }
+  }
+
+  function cekAngka(name) {
+    var cek = /^[a-zA-Z]{1, 50}$/;
+    return cek.test(name);
+  }
+});
